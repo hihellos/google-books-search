5233e7a2464c79248ad94644b41c32bf52aa6d59
hihello <13111181+hihellos@users.noreply.github.com>
2020-09-26 09:49:21 -0400

    updated card and book components

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -3,9 +3,47 @@ import { ListItem } from "../List";
 import { Row, Col } from "../Grid";
 import "./style.css";
 
-function Book(/* YOUR CODE HERE */) {
+function Book(props) {
+  const {
+    title,
+    subtitle,
+    authors,
+    link,
+    description,
+    image,
+    googleId
+  } = props;
   return (
-/* YOUR CODE HERE */  );
+    <ListItem>
+      <Row>
+        <Col size="lg-12">
+          <h3>{title}</h3><h4>{subtitle}</h4>
+          <p>{authors}</p>
+          <small>{googleId}</small>
+        </Col>
+      </Row>
+      <Row>
+        <Col size="lg-3">
+          <img src={image} alt={title}/>
+        </Col>
+        <Col size="lg-8">
+          <p>{description}</p>
+        </Col>
+        <Col size="lg-1">
+          <Row>
+            <Col size="sm-12">
+              <button>Save</button>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="sm-12">
+            <a href={link} target="_blank" rel="noopener noreferrer"><button className="btn btn-primary">View</button></a>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </ListItem>
+  );
 }
 
 export default Book;
