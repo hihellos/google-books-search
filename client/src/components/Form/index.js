ffbff7a2714990a4eb9e16ee5c88fd67e0f062ab
hihello <13111181+hihellos@users.noreply.github.com>
2020-09-25 22:14:36 -0400

    updated form with onclick & onchange actions, updated favicon and page title

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -4,11 +4,10 @@ function Form({ q, handleInputChange, handleFormSubmit }) {
   return (
     <div className="form-group">
       <form>
-        <label>
-          Search Books:
-          <input type="text" name="book"/>
-        </label>
-        <button type="submit" value="Submit"/>
+        <div className="input-group mb-3">
+          <input className="form-control" placeholder="What book would you like to search for?" aria-label="Book name" type="text" name="q" onChange={handleInputChange} />
+        </div>
+        <button className="btn btn-lg btn-primary" type="submit" onClick={handleFormSubmit}>Search <span role="img" aria-label="book emoji">📖</span></button>
       </form>
     </div>
   );
